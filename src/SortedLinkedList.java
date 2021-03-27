de0f048744c8903943cb0ca23b83b7eb20e4fe0a
mwang1534 <69922681+mwang1534@users.noreply.github.com>
2021-03-26 23:13:54 -0700

    Add files via upload

--- src/SortedLinkedList.java
+++ src/SortedLinkedList.java
@@ -1,5 +1,17 @@
 public class SortedLinkedList<T extends Comparable<T>> implements SortedList<T> {
 
+    Node head;
+
+    private class Node<T> {
+        Node<T> next;
+        T data;
+
+        public Node(T value) {
+            this.data = value;
+            next = null;
+        }
+    }
+
     private T[] arr;
     private int size = 0;
 
@@ -10,7 +22,11 @@ public class SortedLinkedList<T extends Comparable<T>> implements SortedList<T> 
 
     @Override
     public boolean add(Comparable x) {
-        return false;
+        Node prev = head;
+        for (int i = 0; i < size; i++) {
+            prev = prev.next;
+            Node
+        }
     }
 
     @Override
@@ -20,11 +36,15 @@ public class SortedLinkedList<T extends Comparable<T>> implements SortedList<T> 
 
     @Override
     public Comparable get(int pos) {
-        return null;
+        Node curr = head;
+        for (int i = 0; i < pos; i++) {
+            curr = curr.next;
+        }
+        return (Comparable) curr.data;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 }
